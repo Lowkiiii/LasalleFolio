e4d68a0f74abe95548fe858ad592bffb9a5618eb
Lowkiiii <lukesellado04@gmail.com>
2024-05-11 17:27:34 +0800

    commit

---
+++ app/Models/UserProject.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProject extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $table = 'user_projects';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'project', 'description', 'date_started', 'date_ended', 'user_id'
+    ];
+
+    
+}
