a954302000444a0d58d6e0a9882b577a3909940d
Lowkiiii <lukesellado04@gmail.com>
2024-05-14 12:18:14 +0800

    Ajax app.css

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,4 +10,26 @@ function toggleModal(modalID) {
     document.getElementById(modalID + "-backdrop").classList.toggle("flex");
 }
 
+// Create anoter toggle method for edit
+function toggleEditModal(modalID, id) {
+    document.getElementById(modalID).classList.toggle("hidden");
+    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+    document.getElementById(modalID).classList.toggle("flex");
+    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+
+    // AJAX request to retrieve the data project id
+    
+    // axios.get(`/projects/${id}`).then((response) => {
+    //     document.getElementById("edit-project-name").value = response.data.name;
+    //     document.getElementById("edit-project-description").value =
+    //         response.data.description;
+    //     document.getElementById("edit-project-id").value = response.data.id;
+    // });
+
+    // console.log(id);
+
+}
+
+
+
 export { toggleModal };
